a59a910c44dcf7a8bca542f61293882533ebdd13
Mimioj101 <mimioj@gmail.com>
2020-12-10 22:07:07 -0500

    byebug says its not permitted

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -3,10 +3,16 @@ require 'rest-client'
 class WikisController < ApplicationController
 
     def wikiarticles
-        url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=30&srsearch=#{params[:search]}&utf8=&format=json"
+        url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=50&srsearch=#{params[:search]}&utf8=&srqiprofile=popular_inclinks_pv&format=json"
         response = RestClient.get(url)
         data = JSON.parse(response.body)
         render json: data
+        byebug
     end
+
+    # private
+    # def wiki_params
+    #     params.require(:wiki).permit(:page_id, :page_title, :snippet, :img, :cat_title, :cat_id)
+    # end
 
 end
